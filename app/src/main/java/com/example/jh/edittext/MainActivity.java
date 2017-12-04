551c98019a7c9553e0104e458906c034594c1d5a
JinHui <hui.jin@boatchina.net>
2017-12-04 19:50:50 +0800

    test

--- app/src/main/java/com/example/jh/edittext/MainActivity.java
+++ app/src/main/java/com/example/jh/edittext/MainActivity.java
@@ -8,24 +8,36 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
- * EditText内容监听变化
+ * EditText 内容监听变化
+ * <p>
+ * 方法一：
+ * 在 xml 文件中设置文本编辑框属性作字符数限制
+ * 如：android:maxLength="10" 即限制最大输入字符个数为10
+ * <p>
+ * 2.
+ * 3.
+ *
+ * 关于多个EditText的光标问题
  */
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-    EditText edit;
+    EditText edit1;
+    EditText edit2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        edit = (EditText) findViewById(R.id.edit);
+        edit1 = (EditText) findViewById(R.id.edit1);
+        edit2 = (EditText) findViewById(R.id.edit2);
         /**
          * EditText编辑框内容发生变化时的监听回调
          */
-        edit.addTextChangedListener(new MyTextChangedListener());
+        edit1.addTextChangedListener(new MyTextChangedListener());
         /**
          *  1、EditText输入框的动态监听方法
          *  A:监听 输入结束点击键盘确认键执行的 方法
@@ -45,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
     /**
      * 文本监听的方法实现
      */
-    public class MyTextChangedListener implements TextWatcher{
+    public class MyTextChangedListener implements TextWatcher {
 
         /**
          * 编辑框的内容发生改变之前的回调方法
@@ -62,14 +74,20 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
             Log.e(TAG, "onTextChanged =" + s.toString());
-        }
+            Log.e(TAG, "s.length() =" + s.length());
+            if (s.length() >= 10){
+                Toast.makeText(MainActivity.this, "输入字符过长，无法再输入", Toast.LENGTH_SHORT).show();
+            }
 
-        /**
-         * 编辑框的内容改变以后,用户没有继续输入时 的回调方法
-         */
-        @Override
-        public void afterTextChanged(Editable s) {
-            Log.e(TAG, "afterTextChanged =" + s.toString());
-        }
+
     }
+
+    /**
+     * 编辑框的内容改变以后,用户没有继续输入时 的回调方法
+     */
+    @Override
+    public void afterTextChanged(Editable s) {
+        Log.e(TAG, "afterTextChanged =" + s.toString());
+    }
+}
 }
