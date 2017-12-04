f94a4a5349fa11a4c6d851b352f05c76cb0deb53
JinHui <hui.jin@boatchina.net>
2017-12-04 16:33:51 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jh/edittext/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.jh.edittext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * EditText内容监听变化
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
